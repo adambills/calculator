ed6533a038fa1b820ffae80b2bb35f9e0691b8e9
a-bills <121698448+a-bills@users.noreply.github.com>
2023-08-13 21:29:06 -0400

    Add operate() function

--- calc.js
+++ calc.js
@@ -24,7 +24,7 @@ function processButton(event) {
         case "multiply": multiply(event); break;
         case "subtract": subtract(event); break;
         case "add": add(event); break;
-        case "equal": equal(event); break;
+        case "equal": operate(event); break;
     }
 }
 
@@ -32,6 +32,11 @@ function appendNumber(event) {
     let endDisplay = false;
     if (display.clientWidth > 330 && display.classList.contains('reduceFont')) {
         endDisplay = true;
+    }
+    if (operator !== null && firstOperand === null) {
+        firstOperand = parseFloat(display.textContent.replaceAll(',', ''));
+        display.textContent = '';
+        display.classList.remove('reduceFont');
     }
     if (!endDisplay) {
         display.textContent += event.target.textContent;
@@ -48,19 +53,10 @@ function appendNumber(event) {
 function clear() {
     display.textContent = '';
     display.classList.remove('reduceFont');
-};
-
-function changeSign() {
-    if (display.textContent.includes('-')) {
-        display.textContent = display.textContent.replace('-', '');
-    } else {
-        display.textContent = `-${display.textContent}`;
-    }
-};
-
-function addDecimal() {
-    if (display.textContent === '') display.textContent += '0.';
-    if (!display.textContent.includes('.')) display.textContent += '.';
+    operator = null;
+    firstOperand = null;
+    secondOperand = null;
+    removeClass();
 }
 
 function backspace(event) {
@@ -71,36 +67,86 @@ function backspace(event) {
     } else display.textContent = '';
 }
 
+function changeSign() {
+    if (display.textContent.includes('-')) {
+        display.textContent = display.textContent.replace('-', '');
+    } else {
+        display.textContent = `-${display.textContent}`;
+    }
+}
+
+function addDecimal() {
+    if (display.textContent === '') display.textContent += '0.';
+    if (!display.textContent.includes('.')) display.textContent += '.';
+}
+
 function divide(event) {
     if (operator === null) {
-        operator === 'divide';
+        operator = 'divide';
         event.target.classList.add('activeOperator');
     }
 }
 
 function multiply(event) {
     if (operator === null) {
-        operator === 'multiply';
+        operator = 'multiply';
         event.target.classList.add('activeOperator');
     }
 }
 
 function subtract(event) {
     if (operator === null) {
-        operator === 'subtract';
+        operator = 'subtract';
         event.target.classList.add('activeOperator');
     }
 }
 
 function add(event) {
     if (operator === null) {
-        operator === 'add';
+        operator = 'add';
         event.target.classList.add('activeOperator');
     }
 }
 
-function equal() {
+function operate(event) {
+    removeClass();
+    secondOperand = parseFloat(display.textContent.replaceAll(',', ''));
+    display.textContent = '';
+    display.classList.remove('reduceFont');
+    let result = null;
+    switch (operator) {
+        case 'divide': result = firstOperand / secondOperand; break;
+        case 'multiply': result = firstOperand * secondOperand; break;
+        case 'subtract': result = firstOperand - secondOperand; break;
+        case 'add': result = firstOperand + secondOperand; break;
+    }
+    if (result.toString().length > 11) {
+        display.textContent = toExponential(result, 3);
+    } else {
+        display.textContent = parseFloat(result).toLocaleString('en-US');
+    }
 
+    if (display.clientWidth > 330 && display.classList.contains('reduceFont')) {
+        endDisplay = true;
+    }
+    if (display.clientWidth > 370) {
+        display.classList.add('reduceFont');
+    } 
+    operator = null;
+    firstOperand = null;
+    secondOperand = null;
+    removeClass();
+}
+
+function toExponential(number, rounding) {
+    return Number.parseFloat(number).toExponential(rounding);
+}
+
+function removeClass() {
+    const buttons = document.querySelectorAll('button');
+    for (const button of buttons) {
+        button.classList.remove('activeOperator')
+    }
 }
 
 activateButtons();
